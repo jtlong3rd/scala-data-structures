70657e1105befd78021fe0367d31176e59293267
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 15:07:14 -0500

    Add List companion object and apply method (with unit tests)

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -2,3 +2,7 @@ sealed trait List[A]
 
 case class Nil[A]() extends List[A]
 case class Cons[A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](args: A*): List[A] = (args foldRight (Nil(): List[A])) ( Cons(_, _) )
+}
