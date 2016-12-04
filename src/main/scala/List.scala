ac190cedba0e0dd2dfa509a2cded922dde1351e1
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 16:02:09 -0500

    Implement foldLeft method and associated unit tests

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,8 +1,17 @@
-sealed trait List[+A]
+sealed trait List[+A] {
+  def foldLeft[B](acc: B)(f: (B, A) => B): B = {
+    def folder(as: List[A], acc: B): B = as match {
+      case Nil => acc
+      case Cons(head, tail) => folder(tail, f(acc, head))
+    }
+
+    folder(this, acc)
+  }
+}
 
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-  def apply[A](args: A*): List[A] = (args foldRight (Nil: List[A])) ( Cons(_, _) )
+  def apply[A](args: A*): List[A] = (args foldRight (Nil: List[A])) (Cons(_, _))
 }
--- src/test/scala/ListTest.scala
+++ src/test/scala/ListTest.scala
@@ -7,4 +7,12 @@ class ListTest extends FunSuite with Matchers {
     List(1, 2) should be ( Cons(1, Cons(2, Nil)) )
     List(1, 2, 3) should be ( Cons(1, Cons(2, Cons(3, Nil))) )
   }
+
+  test("foldLeft") {
+    List().foldLeft(0)((x: Int, y: Int) => x + y) should be ( 0 )
+    List(1).foldLeft(0)(_ + _) should be ( 1 )
+    List(1).foldLeft(2)(_ + _) should be ( 3 )
+    List(1, 2, 3, 4).foldLeft(0)(_ + _) should be ( 10 )
+    List("Jamie", "Lindsey").foldLeft(0)(_ + _.length) should be ( 12 )
+  }
 }
