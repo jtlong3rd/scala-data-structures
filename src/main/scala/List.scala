7b5a473015775db83e2e81dea8e72d9dba34b1d3
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 15:32:09 -0500

    Make element types for List covariant; refactor Nil from a case class to case object

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,8 +1,8 @@
-sealed trait List[A]
+sealed trait List[+A]
 
-case class Nil[A]() extends List[A]
-case class Cons[A](head: A, tail: List[A]) extends List[A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-  def apply[A](args: A*): List[A] = (args foldRight (Nil(): List[A])) ( Cons(_, _) )
+  def apply[A](args: A*): List[A] = (args foldRight (Nil: List[A])) ( Cons(_, _) )
 }
--- src/test/scala/ListTest.scala
+++ src/test/scala/ListTest.scala
@@ -2,9 +2,9 @@ import org.scalatest.{FunSuite, Matchers}
 
 class ListTest extends FunSuite with Matchers {
   test("apply") {
-    List() should be ( Nil() )
-    List(1) should be ( Cons(1, Nil()) )
-    List(1, 2) should be ( Cons(1, Cons(2, Nil())) )
-    List(1, 2, 3) should be ( Cons(1, Cons(2, Cons(3, Nil()))) )
+    List() should be ( Nil )
+    List(1) should be ( Cons(1, Nil ) )
+    List(1, 2) should be ( Cons(1, Cons(2, Nil)) )
+    List(1, 2, 3) should be ( Cons(1, Cons(2, Cons(3, Nil))) )
   }
 }
